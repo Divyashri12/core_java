5a681079e363b1cde9eaa958b9e1f156b64010a2
Divyashri12 <divyashrigk@gmail.com>
2021-09-22 12:05:58 +0530

    writes negation program

---
+++ NegationOperator.java
@@ -0,0 +1,9 @@
+class NegationOperator{
+public static void main (String[] args){
+
+boolean boo=false;
+boolean result=!boo;
+System.out.println("boo:"+boo);
+System.out.println("result:"+result);
+}
+}
