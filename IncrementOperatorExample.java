879dfe0d6a092d060652f1b99ed8524af6d4b385
Divyashri12 <divyashrigk@gmail.com>
2021-09-22 11:58:11 +0530

    writes Increment operator program

---
+++ IncrementOperatorExample.java
@@ -0,0 +1,15 @@
+class IncrementOperatorExample{
+public static void main (String[] args){
+System.out.println("***********pre increment operations ******************");
+int m=7;
+int n=++m;
+System.out.println("m:"+m);
+System.out.println("n:"+n);
+
+System.out.println("******post increment operations******");
+int p=50;
+int q=p++;
+System.out.println("p:"+p);
+System.out.println("q:"+q);
+}
+}
