fcc073503746c95d6e8384e7910f734c4bdf9b11
Divyashri12 <divyashrigk@gmail.com>
2021-09-22 11:53:00 +0530

    writes Arithmatic operator program

---
+++ ArithmaticOperator.java
@@ -0,0 +1,37 @@
+class ArithmaticOperator{
+public static void main(String[] args){
+int m=55;
+int n=65;
+int addResult = m+n;
+System.out.println("addResult:"+addResult);
+
+int x=76;
+int y=42;
+int subResult = x-y;
+System.out.println("subResult : "+subResult);
+
+int l=62;
+int o=8;
+int mulResult = l*o;
+System.out.println("mulResult : "+mulResult);
+int i=5;
+int j=50;
+int divResult = j/i;
+System.out.println("divResult : "+divResult);
+
+int r=35;
+int k=6;
+int modResult=r/k;
+System.out.println("modResult :"+modResult);
+
+}
+}
+
+
+
+
+
+
+
+
+
