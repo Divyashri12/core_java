a6545a4429196e64b40049d482c8db3dcd4a12be
Divyashri12 <divyashrigk@gmail.com>
2021-09-25 20:14:51 +0530

    writes AssignmentOperatorAssignment3 program

---
+++ AssignmentOperatorAssignment3.java
@@ -0,0 +1,12 @@
+class AssignmentOperatorAssignment3{
+public static void main(String[] args){
+int x=100;
+int y=65;
+x+=y;
+System.out.println("x:"+x);
+int i=70;
+int j=7;
+i/=j;
+System.out.println("i:"+i);
+}
+}
