4f7d9c45333ae032e35943f478b69b349a847809
Divyashri12 <divyashrigk@gmail.com>
2021-09-25 20:14:22 +0530

    writes AssignmentOperatorAssignment12 program

---
+++ AssignmentOperatorAssignment2.java
@@ -0,0 +1,12 @@
+class AssignmentOperatorAssignment2{
+public static void main(String[] args){
+int x=75;
+int y=55;
+x-=y;
+System.out.println("x:"+x);
+int i=70;
+int j=7;
+i*=j;
+System.out.println("i:"+i);
+}
+}
