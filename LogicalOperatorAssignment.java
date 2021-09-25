57305ad12d9f249350622947e8ccfd2b8bf3a79d
Divyashri12 <divyashrigk@gmail.com>
2021-09-25 20:22:28 +0530

    writes LogicalOperatorAssignment program

---
+++ LogicalOperatorAssignment.java
@@ -0,0 +1,12 @@
+class LogicalOperatorAssignment{
+public static void main(String[] args){
+int m=55;
+int n=63;
+System.out.println(m<n && m<=n);
+System.out.println(m<n && m>n);
+System.out.println(m==n && m!=n);
+System.out.println(m==n && m>n);
+}
+}
+
+
