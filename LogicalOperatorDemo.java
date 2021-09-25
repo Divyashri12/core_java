ed092364a5a4ce67082355cd5577090106110c1c
Divyashri12 <divyashrigk@gmail.com>
2021-09-25 20:23:47 +0530

    writes LogicalOperatordemo program

---
+++ LogicalOperatorDemo.java
@@ -0,0 +1,12 @@
+class LogicalOperatorDemo{
+public static voidmain(String[] args){
+int a=10;
+int b=20;
+System.out.println(a<b && a<=b);
+System.out.println(a<b && a>b);
+System.out.println(a==b && a!=b);
+System.out.println(a==b && a>b);
+}
+}
+
+
