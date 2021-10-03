b2d8f8725e45479cb492361e20fdf6caa7a712bc
Divyashri12 <divyashrigk@gmail.com>
2021-10-03 16:27:41 +0530

    writes pgm

---
+++ InvalidNumber.java
@@ -0,0 +1,11 @@
+class InvalidNumber{
+public static void main(String[] args){
+int num=20;
+if(num>10){
+System.out.println("The number is valid");
+}
+else{
+System.out.println("The number is invalid");
+}
+}
+}
