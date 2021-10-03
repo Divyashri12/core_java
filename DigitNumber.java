3a880e43c2bf7aec5eb72f280173b61297a43936
Divyashri12 <divyashrigk@gmail.com>
2021-10-03 16:26:21 +0530

    writes pgm

---
+++ DigitNumber.java
@@ -0,0 +1,13 @@
+class DigitNumber{
+public static void main(String[] args){
+long number=123456;
+if(number>10){
+System.out.println("valid Number");
+}
+else if(number<10){
+System.out.println( "invalid number");
+
+}
+}
+}
+
