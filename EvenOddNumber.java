e16d9851c6ff090354742bf39778dc880ebd4334
Divyashri12 <divyashrigk@gmail.com>
2021-10-03 16:27:02 +0530

    writes pgm

---
+++ EvenOddNumber.java
@@ -0,0 +1,11 @@
+class EvenOddNumber{
+public static void main(String[] args){
+int number=7;
+if(number%2==0){
+System.out.println( "Even number");
+}
+else{
+System.out.println(number+ "Odd number");
+}
+}
+}
