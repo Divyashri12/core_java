5645992525dcb050eac035c978ad58c6ed89a366
Divyashri12 <divyashrigk@gmail.com>
2021-10-03 16:29:08 +0530

    writes pgm

---
+++ ToDigitNumber.java
@@ -0,0 +1,11 @@
+class ToDigitNumber{
+public static void main(String[] args){
+int num=16;
+if(num>10 && num<100){
+System.out.println("2 digit number");
+}
+else{
+System.out.println("it is not 2 digit number");
+}
+}
+}
